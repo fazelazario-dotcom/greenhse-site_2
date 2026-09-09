6b91cef303c064d43fcd5396605c2b12db164b86
fazelazario-dotcom <fazelazario@gmail.com>
2026-09-09 17:44:00 +0800

    home

--- app/page.js
+++ app/page.js
@@ -54,5 +54,6 @@ export default function Home(){
     <Script src="/assets/product-urls.js" strategy="afterInteractive"/>
     <Script src="/assets/url-sync4.js" strategy="afterInteractive"/>
     <Script src="/assets/home-app.js" strategy="afterInteractive"/>
+    <Script src="/assets/announce.js" strategy="afterInteractive"/>
   </>);
 }
